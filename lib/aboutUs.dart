9fa5fab6289742a6a023ccac8ca123bebea70d2c
Vincent Zheng <zhengvincent0825@gmail.com>
2020-02-02 19:27:29 -0500

    gmaps image to ios & web

--- lib/aboutUs.dart
+++ lib/aboutUs.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -39,181 +40,194 @@ class _AboutUsState extends State<AboutUs> {
                   ),
                 ),
               ),
-              Text(
-                'We Are',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontFamily: 'UltraCondensedSansSerif',
-                  fontSize: 80.0,
-                  color: Colors.white,
-                ),
-              ),
-              Text(
-                'Polar Ice',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontFamily: 'UltraCondensedSansSerif',
-                  fontSize: 100.0,
-                  color: Colors.blue,
-                ),
-              ),
-              Center(
-                  child: Image.asset(
-                'assets/images/Polar Ice team.png',
-                fit: BoxFit.fitWidth,
-              )),
-              Padding(
-                padding: EdgeInsets.all(15.0),
-                child: Text(
-                  '\"At Polar Ice we work our hardest so our Ice Cream is the best your money can buy. ' +
-                      'We have the perfect ice cream for you no matter what you are looking for. We provide fast frere, ' +
-                      'lactose free, and gluten free ice cream for the best price. Each of our units have been created ' +
-                      'by Andi Tareneshi who sent out many surveys to find the best flavours anyone could want. But ' +
-                      'that is not how it ended our amazing marketing team turned Andi\'s idea to reality creating ' +
-                      'all his flavours exactly the way he wanted it. Here at Polar Ice we care about all our customer ' +
-                      'so we provide the best tasting and healthiest form of ice cream on the market.\"',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontFamily: 'Montserrat',
-                    color: Color(0xffCCCCCCC),
-                    fontSize: 16.0,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.all(15.0),
-                child: Text(
-                  '\"At PolarIce westrive to deliver the best possible ice cream to our customers. We work hard everyday perfecting ' +
-                      'our formulas in order to create the healthy, smooth and delicious treats that fill you with joy ' +
-                      'instead of fat. Each of our departments is specifically tailored to fulfill the needs of each ' +
-                      'and every unit of ice cream that we sell, from our cutting edge research and development ' +
-                      'department led by our intuitive manager Andi tareneshi, to the artistic creativity of our ' +
-                      'marketing department who designs and creates our view of spectacular treats, our company is ' +
-                      'always ready to give it all for the sake of the customer enjoying the astonishing delicacy that ' +
-                      'is PolarIce ice cream.\"',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontFamily: 'Montserrat',
-                    color: Color(0xffCCCCCCC),
-                    fontSize: 16.0,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 20.0),
-                child: Text(
-                  'Location',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontFamily: 'Montserrat',
-                    fontSize: 40.0,
-                    color: Colors.white,
-                    fontWeight: FontWeight.w700,
-                  ),
-                ),
-              ),
               Container(
-                height: 300.0,
-                child: getMap(_center),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 8.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                constraints: BoxConstraints(
+                  maxWidth: 1000,
+                ),
+                child: Column(
                   children: <Widget>[
                     Text(
-                      '456 New Dorp Lane\nStaten Island, New York',
+                      'We Are',
                       textAlign: TextAlign.center,
                       style: TextStyle(
-                        fontSize: 15.0,
+                        fontFamily: 'UltraCondensedSansSerif',
+                        fontSize: 80.0,
                         color: Colors.white,
                       ),
                     ),
-                    GestureDetector(
-                      child: Container(
-                        height: 25.0,
-                        width: 160.0,
-                        decoration: BoxDecoration(
-                            border: Border.all(color: Colors.white, width: 1.0),
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(20.0))),
-                        child: Center(
-                          child: Text(
-                            'Open in Google Maps',
+                    Text(
+                      'Polar Ice',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontFamily: 'UltraCondensedSansSerif',
+                        fontSize: 100.0,
+                        color: Colors.blue,
+                      ),
+                    ),
+                    Center(
+                        child: Image.asset(
+                      'assets/images/Polar Ice team.png',
+                      fit: BoxFit.fitWidth,
+                    )),
+                    Padding(
+                      padding: EdgeInsets.all(15.0),
+                      child: Text(
+                        '\"At Polar Ice we work our hardest so our Ice Cream is the best your money can buy. ' +
+                            'We have the perfect ice cream for you no matter what you are looking for. We provide fast frere, ' +
+                            'lactose free, and gluten free ice cream for the best price. Each of our units have been created ' +
+                            'by Andi Tareneshi who sent out many surveys to find the best flavours anyone could want. But ' +
+                            'that is not how it ended our amazing marketing team turned Andi\'s idea to reality creating ' +
+                            'all his flavours exactly the way he wanted it. Here at Polar Ice we care about all our customer ' +
+                            'so we provide the best tasting and healthiest form of ice cream on the market.\"',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontFamily: 'Montserrat',
+                          color: Color(0xffCCCCCCC),
+                          fontSize: 16.0,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(15.0),
+                      child: Text(
+                        '\"At PolarIce westrive to deliver the best possible ice cream to our customers. We work hard everyday perfecting ' +
+                            'our formulas in order to create the healthy, smooth and delicious treats that fill you with joy ' +
+                            'instead of fat. Each of our departments is specifically tailored to fulfill the needs of each ' +
+                            'and every unit of ice cream that we sell, from our cutting edge research and development ' +
+                            'department led by our intuitive manager Andi tareneshi, to the artistic creativity of our ' +
+                            'marketing department who designs and creates our view of spectacular treats, our company is ' +
+                            'always ready to give it all for the sake of the customer enjoying the astonishing delicacy that ' +
+                            'is PolarIce ice cream.\"',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontFamily: 'Montserrat',
+                          color: Color(0xffCCCCCCC),
+                          fontSize: 16.0,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 20.0),
+                      child: Text(
+                        'Location',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontFamily: 'Montserrat',
+                          fontSize: 40.0,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: 600.0,
+                      child: kIsWeb || Platform.isIOS
+                          ? Image.asset('assets/images/NewDorpHigh.png',
+                              fit: BoxFit.fitHeight)
+                          : getMap(_center),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          Text(
+                            '456 New Dorp Lane\nStaten Island, New York',
+                            textAlign: TextAlign.center,
                             style: TextStyle(
+                              fontSize: 15.0,
                               color: Colors.white,
-                              fontSize: 13.0,
                             ),
+                          ),
+                          GestureDetector(
+                            child: Container(
+                              height: 25.0,
+                              width: 160.0,
+                              decoration: BoxDecoration(
+                                  border:
+                                      Border.all(color: Colors.white, width: 1.0),
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(20.0))),
+                              child: Center(
+                                child: Text(
+                                  'Open in Google Maps',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 13.0,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            onTap: () {
+                              print('hello');
+                              launchMap();
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 35.0),
+                      child: Text(
+                        'Contact Us',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontFamily: 'Montserrat',
+                          fontSize: 40.0,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ),
+                    ListTile(
+                      leading: Icon(
+                        Icons.phone,
+                        color: Colors.white,
+                      ),
+                      title: Text(
+                        '(718) 667-8686',
+                        style: TextStyle(color: Colors.white, fontSize: 17.0),
+                      ),
+                    ),
+                    ListTile(
+                      leading: Icon(
+                        Icons.email,
+                        color: Colors.white,
+                      ),
+                      title: Text(
+                        'Polariceve2020@gmail.com',
+                        style: TextStyle(color: Colors.white, fontSize: 17.0),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 8.0, bottom: 2.5),
+                      child: Center(
+                        child: Text(
+                          'Office hours from 8:00 A.M to 9:36 A.M.',
+                          style: TextStyle(
+                            fontFamily: 'Montserrat',
+                            fontSize: 15.0,
+                            fontStyle: FontStyle.italic,
+                            color: Color(0xffCCCCCCC),
                           ),
                         ),
                       ),
-                      onTap: () {
-                        print('hello');
-                        launchMap();
-                      },
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 2.5, bottom: 30.0),
+                      child: Center(
+                        child: Text(
+                          'Monday through Friday',
+                          style: TextStyle(
+                            fontFamily: 'Montserrat',
+                            fontSize: 15.0,
+                            fontStyle: FontStyle.italic,
+                            color: Color(0xffCCCCCC),
+                          ),
+                        ),
+                      ),
                     ),
                   ],
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 35.0),
-                child: Text(
-                  'Contact Us',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontFamily: 'Montserrat',
-                    fontSize: 40.0,
-                    color: Colors.white,
-                    fontWeight: FontWeight.w700,
-                  ),
-                ),
-              ),
-              ListTile(
-                leading: Icon(
-                  Icons.phone,
-                  color: Colors.white,
-                ),
-                title: Text(
-                  '(718) 667-8686',
-                  style: TextStyle(color: Colors.white, fontSize: 17.0),
-                ),
-              ),
-              ListTile(
-                leading: Icon(
-                  Icons.email,
-                  color: Colors.white,
-                ),
-                title: Text(
-                  'Polariceve2020@gmail.com',
-                  style: TextStyle(color: Colors.white, fontSize: 17.0),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 8.0, bottom: 2.5),
-                child: Center(
-                  child: Text(
-                    'Office hours from 8:00 A.M to 9:36 A.M.',
-                    style: TextStyle(
-                      fontFamily: 'Montserrat',
-                      fontSize: 15.0,
-                      fontStyle: FontStyle.italic,
-                      color: Color(0xffCCCCCCC),
-                    ),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 2.5, bottom: 30.0),
-                child: Center(
-                  child: Text(
-                    'Monday through Friday',
-                    style: TextStyle(
-                      fontFamily: 'Montserrat',
-                      fontSize: 15.0,
-                      fontStyle: FontStyle.italic,
-                      color: Color(0xffCCCCCC),
-                    ),
-                  ),
                 ),
               ),
             ],
@@ -234,26 +248,20 @@ class _AboutUsState extends State<AboutUs> {
   }
 
   getMap(LatLng coords) {
-    if (kIsWeb) {
-      return Center(
-          child: Text('Map on Web Coming Soon...',
-              style: TextStyle(color: Colors.white)));
-    } else {
-      return GoogleMap(
-        zoomGesturesEnabled: true,
-        initialCameraPosition: CameraPosition(
-          target: coords,
-          zoom: 15.0,
+    return GoogleMap(
+      zoomGesturesEnabled: true,
+      initialCameraPosition: CameraPosition(
+        target: coords,
+        zoom: 15.0,
+      ),
+      gestureRecognizers: Set()
+        ..add(Factory<PanGestureRecognizer>(() => PanGestureRecognizer())),
+      markers: Set<Marker>.of([
+        Marker(
+          markerId: MarkerId('New Dorp High School'),
+          position: coords,
         ),
-        gestureRecognizers: Set()
-          ..add(Factory<PanGestureRecognizer>(() => PanGestureRecognizer())),
-        markers: Set<Marker>.of([
-          Marker(
-            markerId: MarkerId('New Dorp High School'),
-            position: coords,
-          ),
-        ]),
-      );
-    }
+      ]),
+    );
   }
 }
